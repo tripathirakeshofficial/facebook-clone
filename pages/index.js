a860fb6379ee65b77a29620068892edde4802c42
tripathirakeshofficial <rakeshtripathi408@gmail.com>
2021-06-07 22:50:26 +0530

    ADD: Facebook Login

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import { getSession } from "next-auth/client";
 import Head from "next/head";
 import Header from "../components/Header";
+import Login from "../components/Login";
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
+
   return (
-    <div>
+    <div className="h-screen bg-gray-100 overflow-hidden">
       <Head>
         <title>Facebook Clone</title>
       </Head>
@@ -17,4 +21,15 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  //Get the User
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
